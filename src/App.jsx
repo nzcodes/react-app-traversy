eace48b3a6fa1fc8ec5bf282f9fbb849d3c872c2
Meowcatt <82116502+Meowcatt@users.noreply.github.com>
2023-05-14 02:20:22 +0600

    reinstalled dependencies after error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import About from './components/About'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -82,11 +85,21 @@ function App() {
   return (
     <div className="container">
       <Header onAdd={() => { setShowAddTask(!showAddTask) }} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
-      {(tasks.length > 0) ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "Nothing to show"}
+      <Route path='/' exact render={(props) => (
+        <>
+          {showAddTask && <AddTask onAdd={addTask} />}
+          {tasks.length > 0 ? (
+            <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+          ) : (
+            "Nothing to show"
+          )}
+        </>
+      )} />
+      <Route path='/about' component={About} />
+      <Footer />
     </div>
-  )
-}
+  );
+}  
 
 export default App;
 

692cd660ca78b6f76199568cb89341d034331428
Meowcatt <82116502+Meowcatt@users.noreply.github.com>
2023-05-14 01:10:16 +0600

    1:36:00

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -5,7 +5,7 @@ const AddTask = ({ onAdd }) => {
   const [day, setDay] = useState('')
   const [reminder, setReminder] = useState(false)
 
-  const onSubmit = (e) =>{
+  const onSubmit = (e) => {
     e.preventDefault()
     if (!text) {
       alert('Please add a task')
@@ -24,23 +24,23 @@ const AddTask = ({ onAdd }) => {
       <div className="form-control">
         <label htmlFor="">Task</label>
         <input type="text" placeholder="Add Task"
-        value={text} onChange={(e) => setText(e.target.value)}
+          value={text} onChange={(e) => setText(e.target.value)}
         />
       </div>
       <div className="form-control">
         <label htmlFor="">Day & Time</label>
         <input type="text" placeholder="Add Day & Time"
-        value={day} onChange={(e) => setDay(e.target.value)}
+          value={day} onChange={(e) => setDay(e.target.value)}
         />
       </div>
       <div className="form-control form-control-check">
         <label htmlFor="">Set Reminder</label>
-        <input type="checkbox" 
-        value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}
+        <input type="checkbox"
+          value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}
         />
       </div>
-      
-      <input type="submit" value="Save Task" className="btn btn-block"/>
+
+      <input type="submit" value="Save Task" className="btn btn-block" />
     </form>
   )
 }
